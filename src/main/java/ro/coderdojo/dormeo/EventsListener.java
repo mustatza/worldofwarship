658d646f8e1cd2395b1eb5e38f7c0b11c06b4551
BuildTools <unconfigured@null.spigotmc.org>
2017-08-16 11:34:19 +0300

    fireball

--- src/main/java/ro/coderdojo/dormeo/EventsListener.java
+++ src/main/java/ro/coderdojo/dormeo/EventsListener.java
@@ -24,25 +24,17 @@ public final class EventsListener implements Listener {
 
 	@EventHandler
 	public void onPlayerThrow(PlayerInteractEvent event) {
+		
 		if (event.getAction() == Action.RIGHT_CLICK_AIR) {
-			if (event.getItem() != null){
-				if (event.getItem().getType() == (Material.FIREBALL)) {
-					Player p = event.getPlayer();
-					Projectile fireball;
-					int fb_speed = 1;
-					int fb_amount = event.getItem().getAmount();
-					final Vector fb_direction = p.getEyeLocation().getDirection().multiply(fb_speed);
-					p.playSound(p.getLocation(), Sound.ENTITY_ENDERMEN_TELEPORT, 10, 1);
-					fireball = p.getWorld().spawn(p.getEyeLocation().add(fb_direction.getX(), fb_direction.getY(), fb_direction.getZ()), Fireball.class);
-					fireball.setShooter((ProjectileSource) p);
-					fireball.setVelocity(fb_direction);
-					if (fb_amount > 1) {
-						event.getItem().setAmount(fb_amount - 1);
-					} else {
-						p.getInventory().clear(p.getInventory().getHeldItemSlot());
-					}
-				}
-			}
+
+				Player p = event.getPlayer();
+				Projectile fireball;
+				int fb_speed = 1;
+				final Vector fb_direction = p.getEyeLocation().getDirection().multiply(fb_speed);
+				p.playSound(p.getLocation(), Sound.ENTITY_ENDERMEN_TELEPORT, 10, 1);
+				fireball = p.getWorld().spawn(p.getEyeLocation().add(fb_direction.getX(), fb_direction.getY(), fb_direction.getZ()), Fireball.class);
+				fireball.setShooter((ProjectileSource) p);
+				fireball.setVelocity(fb_direction);
 
 		}
 	}
